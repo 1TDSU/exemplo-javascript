5286993213a40715bae17acdf22a4a9f3878f799
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2020-05-15 22:45:52 -0300

    Finalizado!

--- js/ex-01.js
+++ js/ex-01.js
@@ -10,15 +10,24 @@ banner2();
 function banner1(){
   document.getElementById('lateral1').src = "./img/banner-1.gif";
   document.getElementById('img-1').href = "http://pudim.com.br";
+  document.getElementById('lateral2').src = "./img/banner-3.gif";
+  document.getElementById('img-2').href = "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6";
+
   setTimeout("banner2()", 4000);
 }
 function banner2(){
   document.getElementById('lateral1').src = "./img/banner-2.gif";
   document.getElementById('img-1').href = "https://fontawesome.com/";
-  setTimeout("banner3()", 3000);
+  document.getElementById('lateral2').src = "./img/banner-3.gif";
+  document.getElementById('img-2').href = "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2015/05/Alpes.jpg";
+
+  setTimeout("banner3()", 4000);
 }
 function banner3(){
   document.getElementById('lateral1').src = "./img/banner-3.gif";
   document.getElementById('img-1').href = "https://unsplash.com/t/business-work";
-  setTimeout("banner1()", 3000);
+  document.getElementById('lateral2').src = "./img/banner-1.gif";
+  document.getElementById('img-2').href = "https://www.hackthebox.eu/";
+
+  setTimeout("banner1()", 4000);
 }
