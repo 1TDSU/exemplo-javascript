c805ecbaf44f0de81cda286f0786d4729ffd4c1c
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2020-05-15 22:41:34 -0300

    Banner 1 finalizado!

--- js/ex-01.js
+++ js/ex-01.js
@@ -5,7 +5,20 @@
   Mais algumas palavras!!!
 */
 
+banner2();
 
 function banner1(){
-  document.getElementById('id').src = "";
+  document.getElementById('lateral1').src = "./img/banner-1.gif";
+  document.getElementById('img-1').href = "http://pudim.com.br";
+  setTimeout("banner2()", 4000);
+}
+function banner2(){
+  document.getElementById('lateral1').src = "./img/banner-2.gif";
+  document.getElementById('img-1').href = "https://fontawesome.com/";
+  setTimeout("banner3()", 3000);
+}
+function banner3(){
+  document.getElementById('lateral1').src = "./img/banner-3.gif";
+  document.getElementById('img-1').href = "https://unsplash.com/t/business-work";
+  setTimeout("banner1()", 3000);
 }
